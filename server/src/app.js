9d77e03f0b8b4fe1f2803aaf1e2ddd66663fca45
igor.znamensky <igor.znamensky@gmail.com>
2024-05-04 15:52:31 +0300

    next 027

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,18 @@ import express from 'express';
 import {apiV1} from "./routes/api.js";
 import morgan from 'morgan';
 import cors from 'cors';
+import ErrorHandler from "./middleware/ErrorHandler.js";
 
 const app = express();
 
+// MIDDLEWARES
 app.use(express.json());
 app.use(morgan('combined'))
 app.use(cors({origin: 'http://localhost:5173'}));
+
+// ERROR HANDLER MIDDLEWARE
+app.use(ErrorHandler)
+
 app.use('/api/v1', apiV1);
 
 
--- server/src/routes/products/product.controller.js
+++ server/src/routes/products/product.controller.js
@@ -1,20 +1,38 @@
 import {Product} from "../../schemas/mongo/product.mongo.js";
 import mongoose from "mongoose";
 
+/**
+ * Get all products
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
 export const getProducts = async (req, res) => {
     const products = await Product.find({})
-    return res.status(200).json({status: "success", products});
+    return res.status(200).json({status: true, products});
 }
 
+/**
+ * Get single product by id
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
 export const getProduct = async (req, res) => {
     let product
     if(mongoose.isValidObjectId(req.params.id)){
         product = await Product.findById(req.params.id)
     }
     if(!product) return res.status(404).json({message: "Product not found"});
-    return res.status(200).json({status: "success", product});
+    return res.status(200).json({status: true, product});
 }
 
+/**
+ * For testing
+ * @param req
+ * @param res
+ * @returns {*}
+ */
 export const testGet = (req, res) => {
     return res.status(200).json({status: true, message: "test"});
 }
